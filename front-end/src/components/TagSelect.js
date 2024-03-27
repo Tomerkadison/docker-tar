7f0c4373b9557b6b9601b85ab055551c349bb799
Tomer <kadison.tomer@gmail.commy>
2024-03-28 01:03:53 +0200

    adding latest tag option for any image

--- front-end/src/components/TagSelect.js
+++ front-end/src/components/TagSelect.js
@@ -1,18 +1,21 @@
 
 import Select from 'react-select';
 
-async function getImageTags(namespace,image){
+async function getImageTags(namespace, image) {
     const imageTagsRes = await fetch(
-        `https://corsproxy.io/?https://hub.docker.com/v2/repositories/${namespace}/${image}/tags/?page_size=100&page=1&name&ordering`
-      )
-    const imageTagsData = await imageTagsRes.json()
-    const imageTags = imageTagsData.results
-    const  imageTagsOptions = imageTags.map(tag => { 
-        return { value: tag.name, label: tag.name };
-    });
-    console.log(imageTagsOptions)
-    return imageTagsOptions
-}
+      `https://corsproxy.io/?https://hub.docker.com/v2/repositories/${namespace}/${image}/tags/?page_size=100&page=1&name&ordering`
+    );
+    const imageTagsData = await imageTagsRes.json();
+    const imageTags = imageTagsData.results;
+  
+    const hasLatestTag = imageTags.some(tag => tag.name === "latest");
+  
+    const imageTagsOptions = hasLatestTag
+      ? imageTags.map(tag => ({ value: tag.name, label: tag.name }))
+      : [{ value: "latest", label: "latest" }, ...imageTags.map(tag => ({ value: tag.name, label: tag.name }))];
+  
+    return imageTagsOptions;
+  }
 
 const TagSelect = (props) => {
     return (

84dabe849fbd6414f917e5aa83a7d1115e8ca68f
Tomer <73030330+Tomerkadison@users.noreply.github.com>
2024-12-30 02:02:50 +0200

    Update App.js

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -78,7 +78,7 @@ function App() {
   function handleDownload() {
     setCurrentPage("LoadingPage")
     downloadFile(
-      `http://34.173.76.241:8080/install/image-tar?image_name=${selectedImage.name}&image_tag=${selectedImageTag}`,
+      `https://dockertar.zapto.org/install?image_name=${selectedImage.name}&image_tag=${selectedImageTag}`,
       `${selectedImage.name}-${selectedImageTag}.tar`
     )
   }

f08f1fbdcbe2dd238f731937ae9486671a494786
Tomer Kadison <tomerkadison@Tomers-MacBook-Air.local>
2025-09-22 18:19:17 +0300

    starting the acount rolling in docker client

--- back-end/docker_client.py
+++ back-end/docker_client.py
@@ -1,3 +1,8 @@
 import docker
+from collections import deque
+
 
 client = docker.from_env(timeout=650)
+
+users = deque(["tomerkad"])
+client.login(username="tomerkad",password="dckr_pat_yaeWjeT3tSeMkVNG-IFRt1b3Yq8")
